3307d7a846ee4e92c6caf57d40414a32de9f1721
gosupark <hpark41@wgu.edu>
2021-03-25 01:46:50 -0700

    Added axios

--- part2/phonebook/src/components/Phonebook.js
+++ part2/phonebook/src/components/Phonebook.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import PhonebookEntry from './PhonebookEntry'
 
-const Phonebook = ({persons}) => {
-
+const Phonebook = ({ persons }) => {
+    
+    const phonebookEntries = () => {
+        if (persons.length !== 0) {
+            return persons.map(person => <PhonebookEntry key={person.name} name={person.name} number={person.phone} />)
+        }
+    }
+    
     return (
         <div>
-            <h2>Numbers</h2>
             <table>
                 <tbody>
-                    {persons.map(person => <PhonebookEntry key={person.name} name={person.name} number={person.phone}/>)}
+                    {phonebookEntries()}
                 </tbody>
             </table>
         </div>
--- part2/phonebook/src/components/PhonebookEntry.js
+++ part2/phonebook/src/components/PhonebookEntry.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const PhonebookEntry = ({id,name,number}) => {
+const PhonebookEntry = ({name, number}) => {
 
     return (
         <tr>
