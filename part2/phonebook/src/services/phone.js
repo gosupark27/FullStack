b204d0a7c021bcf7c95ec857179162287e4b5f02
gosupark <hpark41@wgu.edu>
2021-03-29 20:59:20 -0700

    changed baseUrl

--- part2/phonebook/src/services/phone.js
+++ part2/phonebook/src/services/phone.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // Relative url since front and backend are both on same address
-const baseUrl = '/api/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = async () => {
     const request = await axios
